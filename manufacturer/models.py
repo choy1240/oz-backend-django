d98eeeeacb0b51a1114c667935eb3569c4b0f778
choy1240 <shield3045@naver.com>
2024-09-04 11:49:09 +0900

    add settings

--- manufacturer/models.py
+++ manufacturer/models.py
@@ -3,23 +3,21 @@ from django.conf import settings
 
 # Create your models here.
 class ManuBsn(models.Model):
-    img_url=models.CharField(max_length=2000)
+    img_url=models.URLField(max_length=2000)
     bsn_name=models.CharField(max_length=200)
     reg_no=models.CharField(max_length=50)
     ceo_name=models.CharField(max_length=50)
     user=models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
-        null=True,
         related_name="manu_bsn"
     )
 class ManuAccount(models.Model):
-    img_url=models.CharField(max_length=2000)
+    img_url=models.URLField(max_length=2000)
     bank_name=models.CharField(max_length=50)
     account_no=models.CharField(max_length=200)
     user=models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
-        null=True,
         related_name="manu_account"
     )
--- order/models.py
+++ order/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Order(models.Model):
     title=models.CharField(max_length=200)
     quantity=models.IntegerField()
     description=models.CharField(max_length=200)
-    image_url=models.CharField(max_length=2000)
+    image_url=models.URLField(max_length=2000)
     product_type=models.CharField(max_length=200)
     user=models.ForeignKey(
-        "User",
+        settings.AUTH_USER_MODEL,
         on_delete=models.SET_NULL,
         null=True
     )
