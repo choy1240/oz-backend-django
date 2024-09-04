88a641e973cc52881012e52098db6612cbb19da4
choy1240 <shield3045@naver.com>
2024-09-04 11:31:21 +0900

    add manufactruer

--- manufacturer/models.py
+++ manufacturer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class ManuBsn(models.Model):
@@ -6,7 +7,19 @@ class ManuBsn(models.Model):
     bsn_name=models.CharField(max_length=200)
     reg_no=models.CharField(max_length=50)
     ceo_name=models.CharField(max_length=50)
+    user=models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="manu_bsn"
+    )
 class ManuAccount(models.Model):
     img_url=models.CharField(max_length=2000)
     bank_name=models.CharField(max_length=50)
     account_no=models.CharField(max_length=200)
+    user=models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="manu_account"
+    )
